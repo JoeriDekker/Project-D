ec33362b889de20dd21ce204019c421d444b2777
JoeriDekker <joeridekker2002@gmail.com>
2024-05-07 12:59:00 +0200

    installed tailwind css for react

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
+
+import LoginScreen from './loginscreen';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <LoginScreen />
   </React.StrictMode>
 );
 
