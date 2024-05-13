b10d935201b10ccab351c275c86989d7233470b7
Erik Maan <erikmaan02@gmail.com>
2024-05-13 15:23:46 +0200

    devide src into sections & add page routing

---
+++ client/src/pages/anypage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './index.css';
+
+
+
+function AnyPage() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello PP
+        </h1>
+    );
+}
+
+export default AnyPage;
