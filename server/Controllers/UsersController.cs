462e19d852d956b2ec4418b3f8015c5530dedb4e
nietrenswens <mulderrens@outlook.com>
2024-05-15 13:08:31 +0200

    Adds documentation-site files and adds documentation to userscontroller

--- server/Controllers/UsersController.cs
+++ server/Controllers/UsersController.cs
@@ -6,6 +6,9 @@ using WAMServer.Models;
 
 namespace WAMServer.Controllers
 {
+    /// <summary>
+    /// Controller responsible for handling user-related operations.
+    /// </summary>
     [Authorize]
     [ApiController]
     [Route("/api/[controller]")]
@@ -13,39 +16,55 @@ namespace WAMServer.Controllers
     {
         private readonly IRepository<User> _userRepository;
         private readonly IRepository<Address> _addressRepository;
+
         public UsersController(IRepository<User> userRepository, IRepository<Address> addressRepo)
         {
             _userRepository = userRepository;
             _addressRepository = addressRepo;
         }
 
-
+        /// <summary>
+        /// Retrieves the current user's information.
+        /// </summary>
+        /// <returns>An ActionResult containing the current user's information if authorized; otherwise, returns an UnauthorizedResult.</returns>
         [HttpGet]
         [Authorize]
         public ActionResult<UserDTO> GetUser()
         {
             var currentUser = HttpContext.User;
+            
+            // Check if the user has the required claim
             if (!currentUser.HasClaim(c => c.Type == "Id"))
             {
                 return Unauthorized();
             }
+            
+            // Extract the user id from the claim
             string userId = currentUser.Claims.FirstOrDefault(c => c.Type == "Id")!.Value;
+
+            // Validate the user id
             if (!Guid.TryParse(userId, out Guid id))
             {
                 return Unauthorized();
             }
+
+            // Retrieve the user from the repository
             User? user = _userRepository.Get(id);
+
+            // If user not found, return NotFound
             if (user == null)
             {
                 return NotFound();
             }
-            // If the user has an address, get it from the address repository
+
+            // If the user has an associated address, retrieve it from the address repository
             if (user.AddressId != null)
             {
                 user.Address = _addressRepository.Get(user.AddressId.Value);
             }
+
+            // Return the user DTO as OK response
             return Ok(new UserDTO(user));
         }
-
     }
 }
