361c971d9a9a7a9d918047ad39a9cf926d55a7b6
Erik Maan <87411830+Failfan13@users.noreply.github.com>
2024-05-16 00:51:51 +0200

    Finished interface for connecting minicomputer

--- client/src/components/Button/AnyButton.tsx
+++ client/src/components/Button/AnyButton.tsx
@@ -11,7 +11,7 @@ function AnyButton(props: Props) {
 
 function AnyButton2(props: Props) {
     return (
-        <a href={props.link} className="ring-offset-background mx-5 mt-8 focus-visible:ring-ring flex h-10 w-fit items-center justify-center whitespace-nowrap rounded-md bg-secondaryCol px-8 py-2 text-sm font-medium text-black transition-colors hover:bg-secondaryCol/70 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" type="submit">{props.text}</a>
+        <a href={props.link} className="ring-offset-background mt-8 focus-visible:ring-ring flex h-10 w-fit items-center justify-center whitespace-nowrap rounded-md bg-secondaryCol px-8 py-2 text-sm font-medium text-black transition-colors hover:bg-secondaryCol/70 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" type="submit">{props.text}</a>
     )
 }
 
