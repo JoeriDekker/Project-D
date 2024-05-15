b2456691cdb0ebb864f7547e6a5e9aa0bc155357
JoeriDekker <joeridekker2002@gmail.com>
2024-05-15 08:56:23 +0200

    box shaddow added

--- client/src/pages/logboekscreen.tsx
+++ client/src/pages/logboekscreen.tsx
@@ -13,19 +13,19 @@ function LogboekScreen() {
                 </div>
                 <div className="flex justify-center">
                     <div className="table-auto">
-                        <div className="grid grid-cols-4 gap-2 mt-1">
+                        <div className="grid grid-cols-4 gap-2 mt-2 shadow-md">
                             <div><div className="py-1 px-2 mr-20 rounded-md bg-slate-100"><b>08/12</b></div></div>
                             <div><div className="mr-6">Peil: <text className="text-red-600">-0.12m</text></div></div>
                             <div><div className="mr-6">Min: -0.5m</div></div>
                             <div><div className="mr-6">Max: +0.12m</div></div>
                         </div>
-                        <div className="grid grid-cols-4 gap-2 mt-1">
+                        <div className="grid grid-cols-4 gap-2 mt-2 shadow-md">
                             <div><div className="py-1 px-2 mr-20 rounded-md bg-slate-100"><b>10/12</b></div></div>
                             <div><div className="mr-6">Peil: <text className="text-red-600">-0.12m</text></div></div>
                             <div><div className="mr-6">Min: -0.5m</div></div>
                             <div><div className="mr-6">Max: +0.12m</div></div>
                         </div>
-                        <div className="grid grid-cols-4 gap-2 mt-1">
+                        <div className="grid grid-cols-4 gap-2 mt-2 shadow-md">
                             <div><div className="py-1 px-2 mr-20 rounded-md bg-slate-100"><b>12/12</b></div></div>
                             <div><div className="mr-6">Peil: <text className="text-red-600">-0.12m</text></div></div>
                             <div><div className="mr-6">Min: -0.5m</div></div>
