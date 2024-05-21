18e3033459c52ba1b5b06e1878dffd12bf9623c0
root <root@renswens.nl>
2024-05-21 22:54:27 +0200

    Changed deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+if ! git checkout development; then
+	echo "Could not switch branch"
+	exit -1
+fi
+if ! git pull; then
+	echo "Could not pull"
+	exit -1
+fi
+echo "Stopping containers"
 docker container stop backend
 docker container rm backend
 docker container stop frontend
 docker container rm frontend
 docker rmi project-d_backend
 docker rmi project-d_frontend
-docker-compose up -d
+echo "Rebuilding"
+docker-compose up --build -d
