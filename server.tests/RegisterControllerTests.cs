da8daea9d955f07a4266e443069f7d1576eab0bc
nietrenswens <mulderrens@outlook.com>
2024-05-22 15:35:23 +0200

    Prevents gmail email spam

--- server.tests/RegisterControllerTests.cs
+++ server.tests/RegisterControllerTests.cs
@@ -33,6 +33,7 @@ namespace WAMServer.Tests.Controllers
         {
             _configurationMock.Setup(x => x["Issuer"]).Returns("");
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("");
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
 
             // Arrange
             var userBody = new UserBody { Email = "test@example.com", Password = "Password123!", FirstName = "John", LastName = "Doe" };
@@ -52,6 +53,7 @@ namespace WAMServer.Tests.Controllers
         {
             // Arrange
             _configurationMock.Setup(x => x["JWT:Issuer"]).Returns("http://localhost:5000");
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("http://localhost:3000");
             var userBody = new UserBody { Email = "test@example.com", Password = "Password123!", FirstName = "John", LastName = "Doe" };
             _userRepositoryMock.Setup(x => x.GetAll(It.IsAny<Func<User, bool>>())).Returns(new List<User> { new User(userBody.FirstName, userBody.LastName, userBody.Email, BCrypt.Net.BCrypt.EnhancedHashPassword(userBody.Password)) }.AsQueryable());
@@ -72,6 +74,7 @@ namespace WAMServer.Tests.Controllers
             // Arrange
             _configurationMock.Setup(x => x["JWT:Issuer"]).Returns("http://localhost:5000");
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("http://localhost:3000");
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
             var userBody = new UserBody { Email = "invalid-email", Password = "Password123!", FirstName = "John", LastName = "Doe" };
 
             // Act
@@ -90,6 +93,7 @@ namespace WAMServer.Tests.Controllers
             // Arrange
             _configurationMock.Setup(x => x["JWT:Issuer"]).Returns("http://localhost:5000");
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("http://localhost:3000");
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
             var userBody = new UserBody { Email = "test@example.com", Password = "short", FirstName = "John", LastName = "Doe" };
 
             // Act
@@ -108,6 +112,7 @@ namespace WAMServer.Tests.Controllers
             // Arrange
             _configurationMock.Setup(x => x["JWT:Issuer"]).Returns("http://localhost:5000");
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("http://localhost:3000");
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
             var userBody = new UserBody { Email = "test@example.com", Password = "Password123!", FirstName = "John", LastName = "Doe" };
             var user = new User(userBody.FirstName, userBody.LastName, userBody.Email, userBody.Password);
             _userRepositoryMock.Setup(x => x.GetAll(It.IsAny<Func<User, bool>>())).Returns(new List<User>().AsQueryable());
@@ -126,6 +131,7 @@ namespace WAMServer.Tests.Controllers
             // Arrange
             _configurationMock.Setup(x => x["JWT:Issuer"]).Returns("http://localhost:5000");
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("http://localhost:3000");
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
             // Act
             var result = await _controller.Confirm("invalid-user-id", "some-token");
 
@@ -140,6 +146,7 @@ namespace WAMServer.Tests.Controllers
             _configurationMock.Setup(x => x["JWT:Issuer"]).Returns("http://localhost:5000");
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("http://localhost:3000");
             _userRepositoryMock.Setup(x => x.Get(It.IsAny<Guid>())).Returns<User?>(null!);
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
 
             // Act
             var result = await _controller.Confirm(Guid.NewGuid().ToString(), "some-token");
@@ -156,6 +163,7 @@ namespace WAMServer.Tests.Controllers
             _configurationMock.Setup(x => x["FrontendURL"]).Returns("http://localhost:3000");
             var user = new User { ConfirmationToken = Guid.NewGuid() };
             _userRepositoryMock.Setup(x => x.Get(It.IsAny<Guid>())).Returns(user);
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
 
             // Act
             var result = await _controller.Confirm(user.Id.ToString(), "invalid-token");
@@ -173,6 +181,7 @@ namespace WAMServer.Tests.Controllers
             var user = new User { Id = Guid.NewGuid(), ConfirmationToken = Guid.NewGuid() };
             _userRepositoryMock.Setup(x => x.Get(It.IsAny<Guid>())).Returns(user);
             _userRepositoryMock.Setup(x => x.UpdateAsync(It.IsAny<User>(), It.IsAny<Func<User, bool>>())).ReturnsAsync(new User());
+            _mailServiceMock.Setup(x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
 
             // Act
             var result = await _controller.Confirm(user.Id.ToString(), user.ConfirmationToken.ToString());
