ea229dd96cfba825723e468ac8ac74aa5c17af20
nietrenswens <mulderrens@outlook.com>
2024-06-12 16:58:17 +0200

    Great if fixed

--- server/Controllers/RegisterController.cs
+++ server/Controllers/RegisterController.cs
@@ -14,15 +14,17 @@ namespace WAMServer.Controllers
 
         private IRepository<User> _userRepository;
         private IRepository<Address> _addressRepository;
+        private IRepository<WaterLevelSettings> _waterLevelSettingsRepository;
         private IEmailService _mailService;
         private IConfiguration _configuration;
 
-        public RegisterController(IRepository<User> userRepository, IEmailService mailService, IConfiguration configuration, IRepository<Address> addressRepository)
+        public RegisterController(IRepository<User> userRepository, IEmailService mailService, IConfiguration configuration, IRepository<Address> addressRepository, IRepository<WaterLevelSettings> waterLevelSettingsRepository)
         {
             _userRepository = userRepository;
             _mailService = mailService;
             _configuration = configuration;
             _addressRepository = addressRepository;
+            _waterLevelSettingsRepository = waterLevelSettingsRepository;
         }
 
         [HttpPost]
@@ -61,15 +63,26 @@ namespace WAMServer.Controllers
                 UserId = user.Id
             };
             User registeredUser;
+            WaterLevelSettings waterLevelSettings;
             try
             {
                 registeredUser = await _userRepository.AddAsync(user);
+                waterLevelSettings = await _waterLevelSettingsRepository.AddAsync(
+                new WaterLevelSettings()
+                {
+                    UserId = user.Id,
+                    IdealHeight = -1.85m,
+                    PoleHeight = -2.05m
+                }
+            );
+            await _addressRepository.AddAsync(address);
             } catch (Exception)
             {
                 return BadRequest(new ErrorBody("Register.failed"));
             }
-            await _addressRepository.AddAsync(address);
+            
             user.AddressId = address.Id;
+            user.WaterLevelSettingsId = waterLevelSettings.Id;
             await _userRepository.UpdateAsync(user, u => u.Id == user.Id);
             _mailService.SendEmail(user.Email, "Welcome to WAM", $"We are pleased to hear you want to join the fight against polerot. In order to log in, please confirm your email address by clicking the following link: {frontendURL}/verify/" + user.Id + "/" + user.ConfirmationToken);
             return Ok();
